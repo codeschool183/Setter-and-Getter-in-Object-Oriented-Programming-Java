b81f9a6c1923679586fed3c9c710a0ed64ad08ed
Code School <113282862+codeschool183@users.noreply.github.com>
2022-09-11 14:11:22 +0500

    Add files via upload

---
+++ SetterGetter/Test.java
@@ -0,0 +1,14 @@
+package SetterGetter;
+
+public class Test 
+{
+
+	
+	public static void main(String[] args) 
+	{
+        Student s = new Student();
+        s.setName("Ali");
+        System.out.println("Name: "+s.getName());
+	}
+
+}
